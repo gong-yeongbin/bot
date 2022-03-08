060e8f394f06e9321f54e9b1a14f704521a5eddc
yeongbin-gong <youngbin816@gmail.com>
2022-03-08 13:05:31 +0900

    10시 텔레그램 스케쥴러

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
-import express, { Express } from 'express';
-import loaders from './loaders/index';
+import schedule from 'node-schedule';
+import axios from 'axios';
+import moment from 'moment';
+import dotenv from 'dotenv';
 
-const app: Express = express();
-loaders(app);
+dotenv.config();
 
-const server = app
-  .listen(3000, () => {
-    console.log('3000번 포트에서 서버 실행 중...');
-  })
-  .on('error', (err) => {
-    console.log(err);
-    process.exit(1);
-  });
+// 매일 아침 10시 텔레그램 메세지 전송
+schedule.scheduleJob('0 0 10 * * *', async (cb) => {
+  const chatId: string = `${process.env.TELEGRAM_CHAT_ID}`;
+  const token: string = `${process.env.TELEGRAM_TOKEN}`;
+  const telegram_api: string = `https://api.telegram.org/bot${token}/sendmessage`;
 
-export { server };
+  const message: string = moment().format('YYYY-MM-DD HH:mm:ss');
+  const text: string = encodeURIComponent(message);
+
+  const url: string = `${telegram_api}?chat_id=${chatId}&text=${text}`;
+
+  await axios.get(url);
+});
