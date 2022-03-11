b877a8c54b1b798f2ae7cfcd34c191ef794905d5
yeongbin-gong <youngbin816@gmail.com>
2022-03-11 16:55:59 +0900

    스케쥴러 수정

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,7 +1,9 @@
 import { Express } from 'express';
 import mongooseLoader from './mongoose';
+import scheduler from '../scheduler';
 
 export default async (app: Express) => {
   await mongooseLoader();
   console.log('✌️ MongoDB Connected');
+  scheduler();
 };
